df36d0b0aa7ca822261f24a255462a5a97aeac76
GanaF4ll <ganafall9498@gmail.com>
2024-04-13 00:51:49 +0200

    update and delete routes for formation + swagger doc

--- routes/formationRoute.ts
+++ routes/formationRoute.ts
@@ -42,4 +42,55 @@ router.get("/all", formationController.list_all_formations);
  *         description: Erreur lors de la création de la formation
  */
 router.post("/add", formationController.add_formation);
+
+/**
+ * @swagger
+ * /update/{formationId}:
+ *   put:
+ *     summary: Met à jour une formation existante
+ *     tags: [Formations]
+ *     parameters:
+ *       - in: path
+ *         name: formationId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: L'ID de la formation à mettre à jour
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Formation'
+ *     responses:
+ *       200:
+ *         description: La formation a été mise à jour avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Formation'
+ *       500:
+ *         description: Erreur lors de la mise à jour de la formation
+ */
+router.put("/update/:formationId", formationController.update_a_formation);
+/**
+ * @swagger
+ * /delete/{formationId}:
+ *   delete:
+ *     summary: Supprime une formation existante
+ *     tags: [Formations]
+ *     parameters:
+ *       - in: path
+ *         name: formationId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: L'ID de la formation à supprimer
+ *     responses:
+ *       200:
+ *         description: La formation a été supprimée avec succès
+ *       500:
+ *         description: Erreur lors de la suppression de la formation
+ */
+router.delete("/delete/:formationId", formationController.delete_a_formation);
 export default router;
