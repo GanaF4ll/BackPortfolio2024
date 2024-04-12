072138cf5c102fcb8c505b79ed118d7a704f477d
GanaF4ll <ganafall9498@gmail.com>
2024-04-12 09:28:31 +0200

    z

--- app.ts
+++ app.ts
@@ -6,39 +6,31 @@ import tagRouter from "./routes/tagRoute";
 import certificationRouter from "./routes/certificationRoute";
 import projetRouter from "./routes/projetRoute";
 import { MongoClient, Db, Collection } from "mongodb";
-
 const app = express();
 const port = 4444;
-
 const uri =
   "mongodb+srv://ganafall9498:KpN9Y2x5OjItjDqq@cluster0.xtzcxbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri, {});
-
 mongoose
   .connect(uri, {})
   .then(() => console.log("Connected to MongoDB Cloud"))
   .catch((err) => console.error("Could not connect to MongoDB Cloud", err));
-
 async function main() {
   try {
     await client.connect();
     console.log("Connexion à la base de données réussie");
-
     const database: Db = client.db("Portfolio2024");
     console.log("Base de données sélectionnée :", database.databaseName);
-
     const collections = [
       "projets",
       "formations",
       "competences",
       "certifications",
     ];
-
     const result: Record<string, any> = {};
     for (const collectionName of collections) {
       const collection: Collection = database.collection(collectionName);
       // console.log("Collection sélectionnée :", collection.collectionName);
-
       const data = await collection.find({}).toArray();
       result[collectionName] = data;
     }
@@ -48,27 +40,21 @@ async function main() {
     console.error("Erreur de connexion à la base de données :", error);
   }
 }
-
 main().catch(console.error);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use("/formations", formationRouter);
 app.use("/competences", competenceRouter);
 app.use("/tags", tagRouter);
 app.use("/certifications", certificationRouter);
 app.use("/projets", projetRouter);
-
 // app.listen(port, () => {
 //   console.log(`BackPortfolio app is running on port ${port}`);
 // });
-
 async function startServer() {
   try {
     await mongoose.connect(uri, {});
     console.log("Connected to MongoDB");
-
     app.listen(port, () => {
       console.log(`BackPortfolio app is running on port ${port}`);
     });
@@ -76,5 +62,4 @@ async function startServer() {
     console.error("Failed to connect to MongoDB", error);
   }
 }
-
 startServer();
