643a663e7efe5b73e74209958526f268dcec07f3
GanaF4ll <ganafall9498@gmail.com>
2024-04-14 02:29:24 +0200

    allowedOrigins

--- app.js
+++ app.js
@@ -56,6 +56,24 @@ const app = (0, express_1.default)();
 const port = 4444;
 const uri = process.env.URI;
 const client = new mongodb_1.MongoClient(uri, {});
+
+// Add CORS headers
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:4200",
+    "https://gana-fall.web.app/",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose_1.default
   .connect(uri, {})
   .then(() => console.log("Connected to MongoDB Cloud"))
