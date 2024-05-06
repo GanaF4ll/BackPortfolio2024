b2cff1e73d066cd3c8388673c1f9833361449cb1
GanaF4ll <ganafall9498@gmail.com>
2024-05-06 21:06:07 +0200

    chore: Update app.ts to import Request, Response, and NextFunction from express

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import formationRouter from "./routes/formationRoute";
 import competenceRouter from "./routes/competenceRoute";
